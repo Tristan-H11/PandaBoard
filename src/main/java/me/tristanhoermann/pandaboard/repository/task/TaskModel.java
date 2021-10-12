c8564fa923fe2b8baff21d6fa668f89927d1c863
Tristan <triddel02@gmx.de>
2021-10-12 23:49:17 +0200

    Added getters and setters for TaskModel

--- src/main/java/me/tristanhoermann/pandaboard/repository/task/TaskModel.java
+++ src/main/java/me/tristanhoermann/pandaboard/repository/task/TaskModel.java
@@ -1,13 +1,35 @@
 package me.tristanhoermann.pandaboard.repository.task;
 
-import lombok.Getter;
-import lombok.Setter;
 import me.tristanhoermann.pandaboard.repository.database.DatabaseStub;
 
 /**
  * Represents a {@link TaskModel} with content, title and ID.
  */
 public class TaskModel {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(final String content) {
+        this.content = content;
+    }
 
     private int id;
     private String title;
