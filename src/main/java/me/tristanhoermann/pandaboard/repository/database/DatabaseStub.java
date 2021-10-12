26f7d489672ea9d517e66f49ce99af5561dbe397
Tristan <triddel02@gmx.de>
2021-10-12 23:50:38 +0200

    Deleted not used sub functions. This is going to be removed entirely soon.

--- src/main/java/me/tristanhoermann/pandaboard/repository/database/DatabaseStub.java
+++ src/main/java/me/tristanhoermann/pandaboard/repository/database/DatabaseStub.java
@@ -1,6 +1,5 @@
 package me.tristanhoermann.pandaboard.repository.database;
 
-import lombok.Getter;
 import me.tristanhoermann.pandaboard.repository.task.TaskModel;
 
 import java.util.LinkedList;
@@ -11,7 +10,6 @@ import java.util.LinkedList;
 @Deprecated
 public class DatabaseStub {
 
-    @Getter
     private static final LinkedList<TaskModel> tasks = new LinkedList<>();
 
     /**
@@ -19,18 +17,6 @@ public class DatabaseStub {
      */
     public static void addTask(final TaskModel task){
         tasks.add(task);
-    }
-
-    /**
-     * Returns a task with the given ID. Returns an empty task, when there is none.
-     */
-    public static TaskModel getTaskById(int id){
-        for (TaskModel task : tasks){
-            if(task.getId() == id){
-                return task;
-            }
-        }
-        return TaskModel.getEmptyTask();
     }
 
     /**
