2d04d184c5c61b4a537a4a12626f028addfc20f8
Tristan <triddel02@gmx.de>
2021-10-12 23:45:29 +0200

    Added a handler for data exchange with a mongoDB

---
+++ src/main/java/me/tristanhoermann/pandaboard/repository/database/MongoHandler.java
@@ -0,0 +1,70 @@
+package me.tristanhoermann.pandaboard.repository.database;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import me.tristanhoermann.pandaboard.repository.task.TaskConverter;
+import me.tristanhoermann.pandaboard.repository.task.TaskModel;
+import org.bson.Document;
+
+import java.util.LinkedList;
+
+/**
+ * Handles in- and outgoing queries from or to the database.
+ */
+public class MongoHandler {
+
+    private static MongoClient mongoClient;
+    private static MongoCollection<Document> collection;
+
+    /**
+     * Connects to the database and opens a given database and collection.
+     */
+    public static void connectAndOpenDB(final String database, final String collection) {
+        openConnection();
+        openDatabase(database, collection);
+    }
+
+    /**
+     * Dumps a {@link TaskModel} to the database.
+     */
+    public static void addTask(final TaskModel task) {
+        collection.insertOne(TaskConverter.toDocument(task));
+    }
+
+    /**
+     * Queries the database for a {@link TaskModel} with the given ID and returns it as a {@link Document}.
+     */
+    public static Document getTaskAsDocumentById(final int id) {
+        final BasicDBObject field = new BasicDBObject("_id", id);
+        return collection.find(field).first();
+    }
+
+    /**
+     * Returns a {@link java.util.List} of {@link Document}s containing all tasks existing in the used collection.
+     */
+    public static LinkedList<Document> getAllTasks() {
+        final LinkedList<Document> documentList = new LinkedList<>();
+
+        try (final MongoCursor<Document> cursor = collection.find().iterator()) {
+            while (cursor.hasNext()) {
+                documentList.add(cursor.next());
+            }
+        }
+        return documentList;
+    }
+
+    private static void openConnection() {
+        mongoClient = new MongoClient(
+                new MongoClientURI("mongodb://localhost:27017")
+        );
+    }
+
+    private static void openDatabase(final String databaseName, final String collectionName) {
+        final MongoDatabase database = mongoClient.getDatabase(databaseName);
+        collection = database.getCollection(collectionName);
+    }
+}
