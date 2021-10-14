9da1eb65f5cb3d779302d75e87649413f565a241
Tristan <triddel02@gmx.de>
2021-10-14 22:44:29 +0200

    Added Logging

---
+++ src/main/java/me/tristanhoermann/pandaboard/logging/RequestLoggingFilterConfig.java
@@ -0,0 +1,22 @@
+package me.tristanhoermann.pandaboard.logging;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        final CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
+}
